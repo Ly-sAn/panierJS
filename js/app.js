bd0bdf70dd82ddcf514fc5c15811c7b8cef3aebe
Lucas <lucasgauvain@gmail.com>
2021-02-02 09:30:55 +0100

    typo

--- js/app.js
+++ js/app.js
@@ -12,16 +12,6 @@
 let coursesList = document.querySelector("#courses-list");
 
 
-function ajouteLigne(tableID) {
-  // Récupération d'une référence à la table
-  var refTable = document.getElementById(tableID);
-
-  // Insère une ligne dans la table à l'indice de ligne 0
-  let tr = document.createElement('TR');
-  let td = document.
-
-    t.tBodies[0].appendChild(r)
-}
 
 //créer un objet tr et mettre les td dans le innerHTML
 
@@ -33,16 +23,8 @@ coursesList.addEventListener('click', (e) => {
     let quantity = 1;
     let refTable = document.getElementById("cart-table");
     let tr = document.createElement('TR');
-    let button = "<button class='button'>Supprimer</button>"
-    var newTH = document.createElement('button');
-    newTH.innerHTML = 'Supprimer';
-    let td = `<td></td><td>${title}</td><td>${price}</td><td>${quantity}</td><td>${newTH}</td>`;
+    let button = "<button class='button'>Supprimer</button>";
+    let td = `<td></td><td>${title}</td><td>${price}</td><td>${quantity}</td><td>${button}</td>`;
     refTable.tBodies[0].appendChild(tr).innerHTML += td;
-*
-    newTH.onclick = function () {
-      this.parentElement.removeChild(this);
-    };
-
-
   }
 })
