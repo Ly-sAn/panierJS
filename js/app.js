734b002ced38225157c69d2d112c2917e35682be
Lucas <lucasgauvain@gmail.com>
2021-02-02 17:31:59 +0100

    story 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
-// let boutonPanierUX = document.querySelectorAll(".add-to-cart");
-
-
-// for (let i = 0; i < boutonPanierUX.length; i++)
-// {
-//     boutonPanierUX[i].addEventListener('click', (e) => 
-//     {
-//         e.preventDefault();
-//     })
-// }
-
-
 
 let coursesList = document.querySelector("#courses-list");
-let cartCount = 0;
 
 
 //créer un objet tr et mettre les td dans le innerHTML
@@ -22,31 +9,19 @@ coursesList.addEventListener('click', (e) => {
 
   if (e.target.className == 'add-to-cart') {
     
-    cartCount++;
-    cartCount.toString;
-
     //Récupère le chiffre de data-id de l'élément
     let dataId = e.target.getAttribute("data-id");
     //Parent de l'élément ciblé
     let parent = e.target.parentElement;
 
 
-    let obj = {
-      title : parent.getElementsByTagName("h4")[0].textContent,
-      price : parent.querySelector(".discount").textContent,
-      quantity : 1,
-    }
-
-    //parent.querySelector(".stock").textContent
     let refTable = document.getElementById("cart-table");
     let tr = document.createElement('TR');
     let button = "<button class='button'>Supprimer</button>";
     let td = `<td></td><td>${obj.title}</td><td>${obj.price}</td><td>${obj.quantity}</td><td>${button}</td>`;
     refTable.tBodies[0].appendChild(tr).innerHTML += td;
-    let leftQuantity = parent.querySelector(".stock").textContent;
 
-    window.localStorage.setItem(cartCount, JSON.stringify(COURSES[dataId]) );
-
+    addToLS(COURSES[dataId]);
 
     addEventListener('click', (e) =>{
       if (e.target.className == 'button'){
@@ -62,3 +37,17 @@ coursesList.addEventListener('click', (e) => {
 
   }
 })
+
+function addToLS(data){
+  let a = [];
+  a = localStorage.getItem('panier');
+
+  if (a != null){
+    a = JSON.parse(a);
+  }  else {
+    a = [];
+  }
+  a.push(data);
+  localStorage.setItem('panier', JSON.stringify(a));
+
+}
