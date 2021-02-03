5ecdab9061c74297032319311cf006a726538989
Lucas <lucasgauvain@gmail.com>
2021-02-03 18:16:12 +0100

    maj des stocks mais bug

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ let header = document.querySelector("#header");
 addToCart();
 removeCartItem();
 clearCart();
+// getStock();
+
 
 // Se place dans le container
 coursesList.addEventListener('click', (e) => {
@@ -14,10 +16,12 @@ coursesList.addEventListener('click', (e) => {
     
     //Récupère le chiffre de data-id de l'élément
     let dataId = e.target.getAttribute("data-id");
-
+    
     addToLS(COURSES[dataId]);
 
     addToCart();
+
+    // getStock();
 
   }
 })
@@ -31,6 +35,7 @@ function removeCartItem(){
     
         let lsList = JSON.parse(localStorage.getItem("panier"));
       
+        //Enlever l'objet en question de l'array
         lsList = lsList.filter(item => item !== lsList[index]);
         
         localStorage.setItem('panier', JSON.stringify(lsList));
@@ -39,16 +44,46 @@ function removeCartItem(){
 })
 }
 
+//récupère le stock et l'affiche dans le HTML
+// function getStock(){
+
+//   let lsList = JSON.parse(localStorage.getItem("panier"));
+//   if (lsList == null){
+//     return;
+//   }
+
+//   let AllStocks = document.querySelectorAll(".stock");
+
+//   let ux_ui = lsList.filter(function(item) {
+//     return item.id == 1;
+//   });
+//   let php_8 = lsList.filter(function(item) {
+//     return item.id == 2;
+//   });
+//   let react_js = lsList.filter(function(item) {
+//     return item.id == 3;
+//   });
+//   let node_js = lsList.filter(function(item) {
+//     return item.id == 4;
+//   });
+//   let my_sql = lsList.filter(function(item) {
+//     return item.id == 5;
+//   });
+
+//   AllStocks[0].innerHTML = ux_ui[ux_ui.length-1].stock;
+//   AllStocks[1].innerHTML = php_8[php_8.length-1].stock;
+//   AllStocks[2].innerHTML = react_js[react_js.length-1].stock;
+//   AllStocks[3].innerHTML = node_js[node_js.length-1].stock;
+//   AllStocks[4].innerHTML = my_sql[my_sql.length-1].stock;
+
+// }
 
 function addToCart(){
   
   let lsList = JSON.parse(localStorage.getItem("panier"));
-  console.log(lsList);
-
 
   let button = "<button class='cart-item'>Supprimer</button>";
   let td = "";
-
 
   for(o in lsList){
     td += `<tr class="table-row"><td class="index" style="display: none">${o}</td>
@@ -57,7 +92,6 @@ function addToCart(){
  
   let refTable = document.getElementById("cart-table");
   refTable.tBodies[0].innerHTML = td;
-
 }
 
 
@@ -66,6 +100,7 @@ function clearCart(){
     if (e.target.className == 'button u-full-width'){
       localStorage.clear();
       e.target.parentNode.querySelector(".tbody").innerHTML = "";
+      // getStock();
     }
   })
 }
@@ -80,12 +115,6 @@ function addToLS(data){
     a = JSON.parse(a);
   }  else {
     a = [];
-  }
-
-  if(data.stock > 0){
-    data.stock--;
-  } else {
-    data.stock = 0;
   }
 
   a.push(data);
